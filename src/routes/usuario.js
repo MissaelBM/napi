35dc42017b7c256cdab60d5c97f366e31a35e5b7
missael <alebermor999@gmail.com>
2025-03-05 15:11:31 -0600

    Conección usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -10,8 +10,8 @@ module.exports = (connection) => {
   router.post('/usuario', controller.usuario); 
   router.post('/usuario/login', controller.login)
   router.get('/usuario', authenticateToken(['Administrador']), controller.consultar); 
-  router.get('/usuario/:id', authenticateToken, controller.consultarId); 
-  router.patch('/usuario/:id', authenticateToken, controller.actualizarUsuario); 
+  router.get('/usuario/:id', controller.consultarId); 
+  router.patch('/usuario/:id', controller.actualizarUsuario); 
   router.delete('/usuario/:id', authenticateToken(['Administrador']), controller.eliminarUsuario); 
   router.post('/usuario/refresh-token', controller.refreshToken);
   router.post('/usuario/logout', controller.logout);
